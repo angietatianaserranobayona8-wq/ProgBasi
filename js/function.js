915c7254110aa396c726c3136a8ea2dedb79ad98
angietatianaserranobayona8-wq <angietatianaserranobayona8@gmail.com>
2025-09-09 11:24:34 -0500

    Calcular edad

--- js/function.js
+++ js/function.js
@@ -3,3 +3,25 @@ function CambiarColor(){
     document.body.style.backgroundColor = "black";
     document.getElementById("title").style.color = "orange";
 }
+
+
+function CalcularEdad(){
+    const input = document.getElementById("fecha_nac").value;
+    const resultado = document.getElementById("resultado");
+    
+    if (!input){
+        resultado.textContent= "Por favor ingresa una fecha de nacimiento"
+        return;
+    }
+ const fecha= new Date (input);
+ const today= new Date ();
+
+ let edad = today.getFullYear() -fecha.getFullYear();
+ let mes = today.getMonth() - fecha.getMonth();
+
+ if(mes < 0 || (mes === 0 && today.getDate() < fecha.getDate())){
+    edad--;
+ }
+
+ resultado.textContent = `Tienes ${edad} años`;
+}
---
+++ js/practicajs.js
@@ -0,0 +1,63 @@
+
+// alert("FUCIONAAAA");
+console.log("FUCIONAAAA la consola💻");
+// (3) libreria sweetalert
+// Swal.fire("hola desde Sweetalert 🏐");
+//(4) En el DOM => Modelos de objetos en el documento 
+document.write("Hola desde el dom");
+
+// ######## TIPOS DE VARIABLES 
+ var edad = 19; // alcance global
+ let num =19; // alcance de bloque
+ const nombre = "Angie Serrano"; // no cambia  su valor
+ const pi = 3.14; // mo cambiar valor }
+ const pulgada = 2.54; // mo cambiar valor 
+
+ //###### OPERADORES BASICOS 
+ // Aritmeticos (+ - * / -%)
+ var num_uno = 5;
+ var num_dos = 10;
+ console.log(num_dos + num_uno);
+ console.log(num_dos - num_uno);
+ console.log(num_dos * num_uno);
+ console.log(num_dos / num_uno);
+ console.log(num_dos % num_uno);
+ 
+ //Comparacion 
+ console.log("igual >>>" + (num_dos==num_dos))
+ console.log("Estricto Igual >>>" + (num_dos===num_dos))
+ console.log("Diferente >>>" + (num_dos!=num_dos))
+ console.log("Mayor  >>>" + (num_dos>=num_dos))
+ console.log("Mayor igaul >>>" + (num_dos>=num_dos))
+ console.log("menor >>>" + (num_dos<num_dos))
+ console.log("Manor igaul >>>" + (num_dos<=num_dos))
+
+ // ####### Tipos de Datos 
+ var clase = "prog. basica"  // String
+ var numero = 5 // numero entero
+ var decimal = 2.5 // numero decimal
+ var booleano = true // Boolean ( false/true)
+ var objeto = {nombre: "Angie", edad: 19, Profesion: "Auxiliar de VT"}
+ var array = ["lunes", "martes", "miercoles", "jueves", " viernes"]
+var array_num = [1,2,3,4,5,6,7,8,9];
+var array_mix = [1, "uno", 5, "cinco", 3.5];
+
+let estudiantes = [
+  {nombre:"Angie", edad: 19},
+  {nombre:"Santiago", edad: 21},
+  {nombre:"Leidy", edad: 42},
+  {nombre:"Teo", edad: 10}
+];
+
+let curso= {
+  nombre:"programacion",
+  temas:["html", "css", " js"]
+
+};
+
+console.log(objeto);
+console.log(array);
+console.log(array[3]);
+console.log(estudiantes [1].nombre);
+console.log(curso);
+console.log(curso.temas);
